53f2bf43addb65a23e71e147d8e34a4f4b5e4a5f
bdkopen <bdkopen@gmail.com>
2024-12-05 00:33:19 -0500

    feat: day 3 part 2

--- src/year2024/day03.rs
+++ src/year2024/day03.rs
@@ -21,16 +21,13 @@ fn read_file(filename: &str) -> String {
     return program;
 }
 
+pub fn part1(program_instructions: &str) -> i32 {
 
-pub fn run() {
-    let program_instructions = read_file("input/year2024/day03.txt");
-    
-    let mul_regex_pattern = Regex::new(r"(mul\(([\d]{1,3}),([\d]{1,3})\))").unwrap();
+    let mul_regex_pattern = Regex::new(r"mul\(([\d]{1,3}),([\d]{1,3})\)").unwrap();
 
-    let matches: Vec<(i32, i32)> = mul_regex_pattern
-        .captures_iter(&program_instructions)
+    let matches: Vec<(i32, i32)> = mul_regex_pattern.captures_iter(&program_instructions)
         .map(|mul| {
-            let (_, [_, x, y]) = mul.extract();
+            let (_, [x, y]) = mul.extract();
             return (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap());
         }).collect();
 
@@ -40,4 +37,80 @@ pub fn run() {
     }
 
     println!("{:?}", instruction_result);
+    return instruction_result;
+}
+
+// I chose to address this with a loop rather then more complex Regex for a few reasons.
+// 1. Rust's Regex cargo doesn't support lookarounds which make it nearly impossible to implement.
+// 2. Other Regex cargos allow lookarounds, but prevent non-constant lookaround lengths. Determining
+// multiple lookarounds while also trying to skip invalid characters was proving very complex.
+// 3. While Regex can be short, it's not very maintainable. This loop should be more readable then
+// a very complex Regex.
+pub fn part2(program_instructions: &str) -> i32 {
+
+    let mul_regex_pattern = Regex::new(r"(mul\(([\d]{1,3}),([\d]{1,3})\))").unwrap();
+
+    let mut unprocessed_instructions: &str = program_instructions;
+
+    let mut instructions_enabled: bool = true;
+    let mut instruction_result: i32 = 0;
+
+    while unprocessed_instructions.len() > 0 {
+        let do_instruction = unprocessed_instructions.find("do()");
+
+        if !instructions_enabled {
+            match do_instruction {
+                // Jump to the index where instructions are enabled again and try again.
+                Some(i) => {
+                    unprocessed_instructions = &unprocessed_instructions[i+1..];
+                    instructions_enabled = true;
+                    continue;
+                },
+                // If instructions are off and never turn back on, end early;
+                None => break,
+            }
+        }
+
+        match mul_regex_pattern.captures(unprocessed_instructions) {
+            Some(cap) => {
+                let mul_start = cap.get(0).unwrap().start();
+
+                let dont_instruction = unprocessed_instructions.find("don't()");
+
+                // If the next multiply instruction is farther then the next dont, jump to the dont and loop again.
+                if Some(mul_start) > dont_instruction && dont_instruction != None {
+                    unprocessed_instructions = &unprocessed_instructions[(dont_instruction.unwrap()+1)..];
+                    instructions_enabled = false;
+                    continue;
+                // Otherwise process the next multiply instruction.
+                } else {
+                    let x = cap.get(2).unwrap().as_str().parse::<i32>().unwrap();
+                    let y = cap.get(3).unwrap().as_str().parse::<i32>().unwrap();
+    
+                    instruction_result += x * y;
+
+
+                    unprocessed_instructions = &unprocessed_instructions[(mul_start+1)..];
+
+                    continue;
+                }
+            }
+            // If there are no more multiply instructions, there's no need to keep looping.
+            None => break,
+        }
+
+    }
+
+    println!("{:?}", instruction_result);
+    return instruction_result;
+
+}
+
+pub fn run() -> (i32, i32) {
+    let program_instructions = read_file("input/year2024/day03.txt");
+
+    return (
+        part1(&program_instructions),
+        part2(&program_instructions)
+    );
 }
