a9fadb4ca0ef47883b8380797e065ffca8491c5b
bdkopen <bdkopen@gmail.com>
2024-12-05 23:48:41 -0500

    feat: day 4 part 2

--- src/year2024/day04.rs
+++ src/year2024/day04.rs
@@ -35,17 +35,18 @@ fn part1(word_search: Vec<Vec<char>>) -> i32 {
         while line_length > i {
             // Checking for "X" finds all forward cases.
             // Checking for "S" finds all reverse cases.
+            // All other letters will get reviewed with the "X" or "S" check.
             if line[i] != 'X' && line[i] != 'S' {
                 i += 1;
                 continue;
             }
 
-            let space_right = i + 3 < line_length;
-            let space_up = row_index >= 3;
-            let space_down = row_index + 3 < line_length;
+            let space_available_right = i + 3 < line_length;
+            let space_available_up = row_index >= 3;
+            let space_available_down = row_index + 3 < line_length;
 
             // Forward horizontal case
-            if space_right {
+            if space_available_right {
                 let found_string = line[i].to_string() + &line[i+1].to_string() + &line[i+2].to_string() + &line[i+3].to_string();
                 if found_string == "XMAS" || found_string == "SAMX" {
                     count += 1;
@@ -53,7 +54,7 @@ fn part1(word_search: Vec<Vec<char>>) -> i32 {
             }
 
             // Down vertical case
-            if space_down {
+            if space_available_down {
                 let found_string: String = line[i].to_string()
                     + &word_search[row_index+1][i].to_string()
                     + &word_search[row_index+2][i].to_string()
@@ -64,7 +65,7 @@ fn part1(word_search: Vec<Vec<char>>) -> i32 {
             }
 
             // Diagonal up case
-            if space_right && space_up {
+            if space_available_right && space_available_up {
                 let found_string: String = line[i].to_string()
                     + &word_search[row_index-1][i+1].to_string()
                     + &word_search[row_index-2][i+2].to_string()
@@ -76,7 +77,7 @@ fn part1(word_search: Vec<Vec<char>>) -> i32 {
 
 
             // Diagonal down case
-            if space_right && space_down {
+            if space_available_right && space_available_down {
                 let found_string: String = line[i].to_string()
                     + &word_search[row_index+1][i+1].to_string()
                     + &word_search[row_index+2][i+2].to_string()
@@ -95,8 +96,71 @@ fn part1(word_search: Vec<Vec<char>>) -> i32 {
     return count;
 }
 
-pub fn run() {
+
+// Given a word search, find the total count of instances where the string "MAS" forms a cross
+// Example:
+// M.S
+// .A.
+// M.S
+fn part2(word_search: Vec<Vec<char>>) -> i32 {
+    let mut count = 0;
+
+    let mut row_index = 0;
+    while word_search.len() > row_index {
+        let line = &word_search[row_index];
+
+        let line_length = line.len();
+
+        let mut i = 0;
+
+        while line_length > i {
+            // Check for "A" since it will be the center of the cross.
+            if line[i] != 'A' {
+                i += 1;
+                continue;
+            }
+
+            let space_available_left = i >= 1;
+            let space_available_right = i + 1 < line_length;
+            let space_available_up = row_index >= 1;
+            let space_available_down = row_index + 1 < line_length;
+
+            // If the value overlaps to unavailable space, continue to the next index
+            // because there isn't enough space for a valid index.
+            if !space_available_left || !space_available_right || !space_available_up || !space_available_down {
+                i += 1;
+                continue;
+            }
+
+            let diagnol1: String = word_search[row_index-1][i-1].to_string()
+                + &word_search[row_index+1][i+1].to_string();
+            let diagnol2: String = word_search[row_index+1][i-1].to_string()
+                + &word_search[row_index-1][i+1].to_string();
+
+            // Check that each diagnol corner matches the expected combinations.
+            let valid_diagnol1 = diagnol1 == "MS" || diagnol1 == "SM";
+            let valid_diagnol2 = diagnol2 == "MS" || diagnol2 == "SM";
+
+            if valid_diagnol1 && valid_diagnol2 {
+                count += 1;
+                i += 1;
+                continue;
+            }
+
+            i += 1;
+        }
+
+        row_index += 1;
+    }
+
+    return count;
+}
+
+pub fn run() -> (i32, i32) {
     let word_search = read_file("./input/year2024/day04.txt");
 
-    println!("{:?}", part1(word_search));
+    return (
+        part1(word_search.clone()),
+        part2(word_search)
+    );
 }
