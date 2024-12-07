497bf6547e0706dfb4217f891e97e247d0ff2d62
bdkopen <bdkopen@gmail.com>
2024-12-07 00:32:17 -0500

    feat: day 6 part 1 complete

--- src/year2024/day06.rs
+++ src/year2024/day06.rs
@@ -28,35 +28,40 @@ fn part1(mut grid: Grid<char>) -> u32 {
     let mut initial_coordinates: Option<(usize, usize)> = None;
     for cell in grid.indexed_iter() {
         if cell.1 == &'^' {
-            initial_coordinates = Some((cell.0 .1, cell.0 .1));
+            initial_coordinates = Some(cell.0);
             break;
         }
     }
 
     println!("{:?}", initial_coordinates);
 
-    let (mut column_i, mut row_i) = initial_coordinates.unwrap();
+    let (mut row_i, mut column_i) = initial_coordinates.unwrap();
+
+    // Mark the starting position as visited.
+    step_count += 1;
+    grid[(row_i, column_i)] = 'X';
+
     let mut direction: Direction = Direction::Up;
     loop {
         // Problem: checked_add and checked_sub modify x and y
-        let (next_col_i_option, next_row_i_option) = match direction {
-            Direction::Up => (Some(column_i), Some(row_i + 1)),
-            Direction::Down if row_i > 1 => (Some(column_i), Some(row_i - 1)),
-            Direction::Right => (Some(column_i + 1), Some(row_i)),
-            Direction::Left if column_i > 0 => (Some(column_i - 1), Some(row_i)),
+        let (next_row_i_option, next_col_i_option) = match direction {
+            Direction::Up if row_i > 0 => (Some(row_i - 1), Some(column_i)),
+            Direction::Down => (Some(row_i + 1), Some(column_i)),
+            Direction::Right => (Some(row_i), Some(column_i + 1)),
+            Direction::Left if column_i > 0 => (Some(row_i), Some(column_i - 1)),
             _ => (None, None),
         };
 
-        println!("{:?},{:?}", next_col_i_option, next_row_i_option);
+        println!("{:?},{:?}", next_row_i_option, next_col_i_option);
 
         // If there is no next grid value, we are outside of the grid bounds.
         if next_col_i_option == None || next_row_i_option == None {
             break;
         }
 
-        let (next_col_i, next_row_i) = (next_col_i_option.unwrap(), next_row_i_option.unwrap());
+        let (next_row_i, next_col_i) = (next_row_i_option.unwrap(), next_col_i_option.unwrap());
 
-        let grid_next_option = grid.get(row_i, column_i);
+        let grid_next_option = grid.get(next_row_i, next_col_i);
 
         if grid_next_option == None {
             break;
@@ -81,17 +86,19 @@ fn part1(mut grid: Grid<char>) -> u32 {
         // We haven't been to the new space, so we increment the count.
         if grid_next != &'X' {
             step_count += 1;
+            grid[(next_row_i, next_col_i)] = 'X';
         }
 
         println!(
-            "final: {:?}{:?}",
+            "previous: {:?},final: {:?}",
             (row_i, column_i),
-            (next_col_i, next_row_i)
+            (next_row_i, next_col_i)
         );
 
-        grid[(row_i, column_i)] = 'X';
-        (row_i, column_i) = (next_col_i, next_row_i);
+        (row_i, column_i) = (next_row_i, next_col_i);
     }
+
+    println!("{:?}", grid);
 
     return step_count;
 }
