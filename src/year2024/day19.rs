aa489e9f06b1cce8e7aa578db2c8e55d534f9b01
bdkopen <bdkopen@gmail.com>
2025-02-28 20:33:52 -0500

    year2024: day 19 part 2

--- src/year2024/day19.rs
+++ src/year2024/day19.rs
@@ -1,5 +1,5 @@
 use crate::util::file::read;
-use std::{cmp::Ordering, collections::BinaryHeap, collections::HashSet};
+use std::collections::HashMap;
 fn process_file(filename: &str) -> (Vec<String>, Vec<String>) {
     let mut input_iter = read(filename).unwrap().flatten();
 
@@ -18,20 +18,7 @@ fn process_file(filename: &str) -> (Vec<String>, Vec<String>) {
     return (towel_patterns, desired_patterns);
 }
 
-#[derive(Eq, PartialEq, Hash)]
-struct Strand(String);
-impl Ord for Strand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        (self.0.len()).cmp(&(&other.0.len()))
-    }
-}
-impl PartialOrd for Strand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn part1(mut towel_patterns: Vec<String>, desired_patterns: Vec<String>) -> usize {
+fn part1(mut towel_patterns: Vec<String>, desired_patterns: Vec<String>) -> Vec<u64> {
     // Sort the largest patterns first. For a greedy algorithm, we'll try the largest patterns first.
     towel_patterns.sort_by(|pattern1, pattern2| pattern1.len().cmp(&pattern2.len()));
 
@@ -39,45 +26,75 @@ fn part1(mut towel_patterns: Vec<String>, desired_patterns: Vec<String>) -> usiz
         .iter()
         .map(|pattern| {
             // Perform Dijkstra's algorithm to determine if the towel pattern can be made.
-            let mut visited: HashSet<Strand> = HashSet::new();
-            let mut queue: BinaryHeap<Strand> = BinaryHeap::new();
-            queue.push(Strand(String::new()));
+            let mut visited: HashMap<String, u64> = HashMap::new();
+            let mut queue: Vec<(String, String)> = Vec::new();
+            queue.push((String::new(), String::new()));
 
-            while let Some(strand) = queue.pop() {
-                for i in 0..towel_patterns.len() {
-                    let mut new_strand = strand.0.to_string();
-                    new_strand.push_str(&towel_patterns[i].to_string());
+            while let Some((towel, new_strand)) = queue.pop() {
+                // println!("{:?}", strand.0);
+                // println!("{:?}", queue);
 
-                    // If we've checked this strand before or the strand doesn't match the towel, skip.
-                    if !visited.insert(Strand(new_strand.clone())) {
-                        continue;
-                    }
-                    if new_strand.len() > pattern.len() {
-                        continue;
-                    }
-                    if pattern[0..new_strand.len()] != new_strand {
-                        continue;
-                    }
+                let towel_string = towel.clone();
+                let mut new_strand_string = towel_string.clone();
+                new_strand_string.push_str(&new_strand);
 
-                    if pattern == &new_strand {
-                        return true;
-                    }
+                let previous_count = match visited.get(&towel) {
+                    Some(&x) => x,
+                    None => 1,
+                };
 
-                    queue.push(Strand(new_strand));
+                let count = match visited.get(&new_strand_string) {
+                    Some(x) => x + previous_count,
+                    None => previous_count,
+                };
+
+                // println!("{}: {}", new_strand_string, count);
+
+                if visited.insert(new_strand_string.clone(), count).is_some() {
+                    continue;
                 }
+                if new_strand_string.len() > pattern.len() {
+                    continue;
+                }
+                if pattern[0..new_strand_string.len()] != new_strand_string {
+                    continue;
+                }
+
+                for i in 0..towel_patterns.len() {
+                    queue.push((new_strand_string.clone(), towel_patterns[i].clone()));
+                }
+
+                queue.sort_by(|pattern1, pattern2| {
+                    (pattern2.0.len() + pattern2.1.len())
+                        .cmp(&(&pattern1.0.len() + pattern1.1.len()))
+                });
+                // println!("{:?}", queue);
             }
 
-            return false;
+            println!("set complete");
+            // println!("{:?}", visited);
+
+            let result = match visited.get(pattern) {
+                Some(x) => x.clone(),
+                None => 0,
+            };
+
+            // println!("{}", result);
+
+            return result;
         })
-        .filter(|&value| value)
-        .collect::<Vec<_>>()
-        .len();
+        .filter(|&value| value > 0)
+        .collect::<Vec<u64>>();
 }
 
 pub fn run() {
     let (towel_patterns, desired_patterns) = process_file("input/year2024/day19.txt");
 
-    let part1_result = part1(towel_patterns, desired_patterns);
+    let result = part1(towel_patterns, desired_patterns);
+
+    let part1_result = result.len();
+    let part2_result: u64 = result.iter().sum();
 
     println!("Part 1: {}", part1_result);
+    println!("Part 2: {}", part2_result);
 }
