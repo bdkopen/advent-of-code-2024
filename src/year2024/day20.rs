c76ec79863e10303a8e1098acad2031a1d6a17ee
bdkopen <bdkopen@gmail.com>
2025-03-08 09:19:50 -0500

    chore: year 2024 day 20 part 1 cleanup

--- src/year2024/day20.rs
+++ src/year2024/day20.rs
@@ -22,86 +22,88 @@ struct Location {
     distance: u16,
 }
 
-fn part1(maze: &Grid<char>, time_save: u16) -> u16 {
-    let mut remaining_distance_cache: HashMap<Point, u16> = HashMap::new();
+fn bfs(
+    maze: &Grid<char>,
+    start_location: Location,
+    remaining_distance_cache: &mut HashMap<Point, u16>,
+) -> Vec<Location> {
+    let mut visited: HashSet<Point> = HashSet::new();
+    let mut queue: Vec<(Location, Vec<Location>)> = Vec::new();
 
-    let bfs = |start_location: Location,
-               remaining_distance_cache: &mut HashMap<Point, u16>|
-     -> Vec<Location> {
-        let mut visited: HashSet<Point> = HashSet::new();
-        let mut queue: Vec<(Location, Vec<Location>)> = Vec::new();
+    queue.push((start_location, vec![start_location.clone()]));
 
-        queue.push((start_location, vec![start_location.clone()]));
+    let final_path = loop {
+        let (Location { point, distance }, mut path) =
+            queue.pop().expect("Queue must have a value");
 
-        let final_path = loop {
-            let (Location { point, distance }, mut path) =
-                queue.pop().expect("Queue must have a value");
+        if !visited.insert(point) {
+            continue;
+        }
 
-            if !visited.insert(point) {
-                continue;
-            }
+        if let Some(&cached_distance) = remaining_distance_cache.get(&point) {
+            let last_index = path.len() - 1;
+            let mut location = path[last_index];
+            location.distance = distance + cached_distance;
+            path[last_index] = location;
 
-            if let Some(&cached_distance) = remaining_distance_cache.get(&point) {
-                let last_index = path.len() - 1;
-                let mut location = path[last_index];
-                location.distance = distance + cached_distance;
-                path[last_index] = location;
+            return path;
+        }
 
-                return path;
-            }
+        let location_value = maze[point];
 
-            let location_value = maze[point];
+        if location_value == 'E' {
+            break path;
+        }
 
-            if location_value == 'E' {
-                break path;
-            }
+        // Push adjacent locations
+        [
+            (point.x.checked_sub(1), Some(point.y)),
+            (Some(point.x), point.y.checked_sub(1)),
+            (point.x.checked_add(1), Some(point.y)),
+            (Some(point.x), point.y.checked_add(1)),
+        ]
+        .iter()
+        .for_each(|(x, y)| {
+            let location_value = maze.checked_get(y, x);
 
-            // Push adjacent locations
-            [
-                (point.x.checked_sub(1), Some(point.y)),
-                (Some(point.x), point.y.checked_sub(1)),
-                (point.x.checked_add(1), Some(point.y)),
-                (Some(point.x), point.y.checked_add(1)),
-            ]
-            .iter()
-            .for_each(|(x, y)| {
-                let location_value = maze.checked_get(y, x);
+            if location_value.is_some() && ['.', 'E'].contains(location_value.unwrap()) {
+                let new_location = Location {
+                    point: Point {
+                        x: x.unwrap(),
+                        y: y.unwrap(),
+                    },
+                    distance: distance + 1,
+                };
+                let mut new_path = path.clone();
+                new_path.push(new_location.clone());
 
-                if location_value.is_some() && ['.', 'E'].contains(location_value.unwrap()) {
-                    let new_location = Location {
-                        point: Point {
-                            x: x.unwrap(),
-                            y: y.unwrap(),
-                        },
-                        distance: distance + 1,
-                    };
-                    let mut new_path = path.clone();
-                    new_path.push(new_location.clone());
-
-                    queue.push((new_location, new_path))
-                }
-            });
-
-            // Sort the queue by distance.
-            queue.sort_by(|(location1, _), (location2, _)| {
-                (location1.distance).cmp(&location2.distance)
-            });
-        };
-
-        // Update the cache.
-        let total_distance = final_path[final_path.len() - 1].distance;
-        final_path.iter().for_each(|Location { point, distance }| {
-            let remaining_distance = total_distance - distance;
-
-            // Get the point value from the cache, if it exists.
-            let cache_value = remaining_distance_cache.get(point);
-            if cache_value.is_none() || Some(&remaining_distance) > cache_value {
-                remaining_distance_cache.insert(point.clone(), remaining_distance);
+                queue.push((new_location, new_path))
             }
         });
 
-        return final_path;
+        // Sort the queue by distance.
+        queue.sort_by(|(location1, _), (location2, _)| {
+            (location1.distance).cmp(&location2.distance)
+        });
     };
+
+    // Update the cache.
+    let total_distance = final_path[final_path.len() - 1].distance;
+    final_path.iter().for_each(|Location { point, distance }| {
+        let remaining_distance = total_distance - distance;
+
+        // Get the point value from the cache, if it exists.
+        let cache_value = remaining_distance_cache.get(point);
+        if cache_value.is_none() || Some(&remaining_distance) > cache_value {
+            remaining_distance_cache.insert(point.clone(), remaining_distance);
+        }
+    });
+
+    return final_path;
+}
+
+fn part1(maze: &Grid<char>, time_save: u16) -> u16 {
+    let mut remaining_distance_cache: HashMap<Point, u16> = HashMap::new();
 
     // Perform a breadth first search to find the shortest. Along the way, record the path.
     let start = maze
@@ -113,19 +115,15 @@ fn part1(maze: &Grid<char>, time_save: u16) -> u16 {
         distance: 0,
     };
 
-    let bfs_path = bfs(start_location, &mut remaining_distance_cache);
+    let bfs_path = bfs(maze, start_location, &mut remaining_distance_cache);
     let bfs_distance = bfs_path[bfs_path.len() - 1].distance;
-
-    // println!("{:?} - len: {}", bfs_path, bfs_path.len());
 
     let mut count = 0;
 
     for i in 0..bfs_path.len() {
         let Location { point, distance } = bfs_path[i];
 
-        println!("{} of {}", i, bfs_path.len());
-
-        // find valid cheats
+        // Find all valid cheats
         [
             (
                 (point.x.checked_sub(1), Some(point.y)),
@@ -156,6 +154,7 @@ fn part1(maze: &Grid<char>, time_save: u16) -> u16 {
 
             if start_value == Some(&'E') || start_value == Some(&'.') {
                 let bfs_cheat_distance = bfs(
+                    maze,
                     Location {
                         point: Point {
                             x: start_x.unwrap(),
