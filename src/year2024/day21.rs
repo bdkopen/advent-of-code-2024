c3cfb7ecb4ac4bb87c99429935a4e320f40b44c9
bdkopen <bdkopen@gmail.com>
2025-03-30 16:00:44 -0400

    feat: year 2024 day 21 part 2 simplify human control layer

--- src/year2024/day21.rs
+++ src/year2024/day21.rs
@@ -61,7 +61,7 @@ fn find_numeric_keypad_moves(input: &str) -> Vec<String> {
 const DIRECTIONAL_KEYPAD: [char; 6] = ['<', 'v', '>', ' ', '^', 'A'];
 const DIRECTIONAL_KEYPAD_START_INDEX: usize = 5;
 
-fn find_directional_keypad_moves(input: &str, human_input: bool) -> Vec<String> {
+fn find_directional_keypad_moves(input: &str) -> Vec<String> {
     let directional_keypad_map: HashMap<char, usize> = HashMap::from_iter(
         DIRECTIONAL_KEYPAD
             .iter()
@@ -81,37 +81,21 @@ fn find_directional_keypad_moves(input: &str, human_input: bool) -> Vec<String> 
 
             let current_row = index / 3;
             let current_col = index % 3;
-            if human_input {
-                if desired_col < current_col && index != 4 {
-                    index -= 1;
-                    intermediate.push('<');
-                } else if desired_row < current_row {
-                    index -= 3;
-                    intermediate.push('v');
-                } else if desired_col > current_col {
-                    index += 1;
-                    intermediate.push('>');
-                } else if desired_row > current_row && index != 0 {
-                    index += 3;
-                    intermediate.push('^');
-                }
-            } else {
-                if desired_row < current_row && desired_col == 0 {
-                    index -= 3;
-                    intermediate.push('v');
-                } else if desired_col > current_col {
-                    index += 1;
-                    intermediate.push('>');
-                } else if desired_col < current_col && index != 4 {
-                    index -= 1;
-                    intermediate.push('<');
-                } else if desired_row > current_row && index != 0 {
-                    index += 3;
-                    intermediate.push('^');
-                } else if desired_row < current_row {
-                    index -= 3;
-                    intermediate.push('v');
-                }
+            if desired_row < current_row && desired_col == 0 {
+                index -= 3;
+                intermediate.push('v');
+            } else if desired_col > current_col {
+                index += 1;
+                intermediate.push('>');
+            } else if desired_col < current_col && index != 4 {
+                index -= 1;
+                intermediate.push('<');
+            } else if desired_row > current_row && index != 0 {
+                index += 3;
+                intermediate.push('^');
+            } else if desired_row < current_row {
+                index -= 3;
+                intermediate.push('v');
             }
         }
         intermediate.push('A');
@@ -144,7 +128,7 @@ fn calculate_sequence(inputs: &Vec<String>, directional_keypad_count: u8) -> usi
                                 v.extend(value.clone());
                                 return v;
                             }
-                            let value = find_directional_keypad_moves(&sub_sequence, false);
+                            let value = find_directional_keypad_moves(&sub_sequence);
                             v.extend(value.clone());
                             directional_keypad_cache.insert(sub_sequence, value);
                             return v;
@@ -153,22 +137,12 @@ fn calculate_sequence(inputs: &Vec<String>, directional_keypad_count: u8) -> usi
                 loop_index += 1;
             }
 
-            let human_keypad_sequence = directional_keypad_sequence
-                .into_iter()
-                .fold(vec![], |mut v, sub_sequence| {
-                    if let Some(value) = directional_keypad_cache.get(&sub_sequence) {
-                        v.extend(value.clone());
-                        return v;
-                    }
-                    let value = find_directional_keypad_moves(&sub_sequence, true);
-                    directional_keypad_cache.insert(sub_sequence, value.clone());
-                    v.extend(value.clone());
-                    return v;
-                })
-                .into_iter()
-                .fold(String::new(), |s, sequence| {
-                    return s + &sequence;
-                });
+            let human_keypad_sequence =
+                directional_keypad_sequence
+                    .into_iter()
+                    .fold(String::new(), |s, sequence| {
+                        return s + &sequence;
+                    });
 
             return number * human_keypad_sequence.len();
         })
@@ -177,14 +151,14 @@ fn calculate_sequence(inputs: &Vec<String>, directional_keypad_count: u8) -> usi
 
 pub fn run() {
     let inputs = process_file("input/year2024/day21-test.txt");
-    let part1_result = calculate_sequence(&inputs, 1);
+    let part1_result = calculate_sequence(&inputs, 2);
     println!("Part 1: {}", part1_result);
-    let part2_result = calculate_sequence(&inputs, 18);
-    println!("Part 2: {}", part2_result);
+    // let part2_result = calculate_sequence(&inputs, 26);
+    // println!("Part 2: {}", part2_result);
 
     let inputs = process_file("input/year2024/day21.txt");
-    let part1_result = calculate_sequence(&inputs, 1);
+    let part1_result = calculate_sequence(&inputs, 2);
     println!("Part 1: {}", part1_result);
-    let part2_result = calculate_sequence(&inputs, 10);
-    println!("Part 2: {}", part2_result);
+    // let part2_result = calculate_sequence(&inputs, 26);
+    // println!("Part 2: {}", part2_result);
 }
