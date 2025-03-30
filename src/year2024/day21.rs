a3fef8fe2f21836f6549fe47919dcf5e0f3e1d76
bdkopen <bdkopen@gmail.com>
2025-03-30 16:39:31 -0400

    improve: year 2024 day 21 cache partial working

--- src/year2024/day21.rs
+++ src/year2024/day21.rs
@@ -61,7 +61,27 @@ fn find_numeric_keypad_moves(input: &str) -> Vec<String> {
 const DIRECTIONAL_KEYPAD: [char; 6] = ['<', 'v', '>', ' ', '^', 'A'];
 const DIRECTIONAL_KEYPAD_START_INDEX: usize = 5;
 
-fn find_directional_keypad_moves(input: &str) -> Vec<String> {
+fn find_directional_keypad_moves(
+    input: &str,
+    iteration: u8,
+    max_iteration: u8,
+    directional_keypad_cache: &mut HashMap<ComputedSequence, usize>,
+) -> usize {
+    // If we've reached the final iteration, return the length of the input.
+    if iteration == max_iteration {
+        return input.len();
+    }
+
+    let computed_sequence = ComputedSequence {
+        input: input.to_string(),
+        iteration: iteration,
+    };
+
+    // Check the cache to avoid looking up previously determined results.
+    if let Some(&values) = directional_keypad_cache.get(&computed_sequence) {
+        return values;
+    }
+
     let directional_keypad_map: HashMap<char, usize> = HashMap::from_iter(
         DIRECTIONAL_KEYPAD
             .iter()
@@ -70,7 +90,6 @@ fn find_directional_keypad_moves(input: &str) -> Vec<String> {
     );
 
     let mut moves = vec![];
-
     let mut index = DIRECTIONAL_KEYPAD_START_INDEX;
 
     input.chars().for_each(|input_char| {
@@ -102,11 +121,31 @@ fn find_directional_keypad_moves(input: &str) -> Vec<String> {
         moves.push(intermediate);
     });
 
-    return moves;
+    let length = moves
+        .iter()
+        .map(|input| {
+            find_directional_keypad_moves(
+                input,
+                iteration + 1,
+                max_iteration,
+                directional_keypad_cache,
+            )
+        })
+        .sum();
+
+    directional_keypad_cache.insert(computed_sequence, length);
+
+    return length;
 }
 
-fn calculate_sequence(inputs: &Vec<String>, directional_keypad_count: u8) -> usize {
-    let mut directional_keypad_cache: HashMap<String, Vec<String>> = HashMap::new();
+#[derive(Eq, PartialEq, Hash)]
+struct ComputedSequence {
+    input: String,
+    iteration: u8,
+}
+
+fn calculate_sequence(inputs: &Vec<String>, max_iteration: u8) -> usize {
+    let mut directional_keypad_cache: HashMap<ComputedSequence, usize> = HashMap::new();
 
     inputs
         .iter()
@@ -117,48 +156,35 @@ fn calculate_sequence(inputs: &Vec<String>, directional_keypad_count: u8) -> usi
 
             let numeric_keypad_sequence = find_numeric_keypad_moves(input);
 
-            let mut loop_index = 0;
-            let mut directional_keypad_sequence = numeric_keypad_sequence;
-            while loop_index < directional_keypad_count {
-                directional_keypad_sequence =
-                    directional_keypad_sequence
-                        .into_iter()
-                        .fold(vec![], |mut v, sub_sequence| {
-                            if let Some(value) = directional_keypad_cache.get(&sub_sequence) {
-                                v.extend(value.clone());
-                                return v;
-                            }
-                            let value = find_directional_keypad_moves(&sub_sequence);
-                            v.extend(value.clone());
-                            directional_keypad_cache.insert(sub_sequence, value);
-                            return v;
-                        });
+            let length: usize = numeric_keypad_sequence
+                .iter()
+                .map(|input| {
+                    find_directional_keypad_moves(
+                        input,
+                        0,
+                        max_iteration,
+                        &mut directional_keypad_cache,
+                    )
+                })
+                .sum();
 
-                loop_index += 1;
-            }
-
-            let human_keypad_sequence =
-                directional_keypad_sequence
-                    .into_iter()
-                    .fold(String::new(), |s, sequence| {
-                        return s + &sequence;
-                    });
-
-            return number * human_keypad_sequence.len();
+            return number * length;
         })
         .sum()
 }
 
+const MAX_ITERATION: u8 = 26;
+
 pub fn run() {
-    let inputs = process_file("input/year2024/day21-test.txt");
-    let part1_result = calculate_sequence(&inputs, 2);
-    println!("Part 1: {}", part1_result);
-    // let part2_result = calculate_sequence(&inputs, 26);
+    // let inputs = process_file("input/year2024/day21-test.txt");
+    // let part1_result = calculate_sequence(&inputs, 2);
+    // println!("Part 1: {}", part1_result);
+    // let part2_result = calculate_sequence(&inputs, MAX_ITERATION);
     // println!("Part 2: {}", part2_result);
 
     let inputs = process_file("input/year2024/day21.txt");
     let part1_result = calculate_sequence(&inputs, 2);
     println!("Part 1: {}", part1_result);
-    // let part2_result = calculate_sequence(&inputs, 26);
-    // println!("Part 2: {}", part2_result);
+    let part2_result = calculate_sequence(&inputs, MAX_ITERATION);
+    println!("Part 2: {}", part2_result);
 }
