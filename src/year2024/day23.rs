300d5d2c93517da57a4b32333bbd85d922460da8
bdkopen <bdkopen@gmail.com>
2025-06-01 17:52:01 -0400

    feat: year 2024 day 23 clean up

--- src/year2024/day23.rs
+++ src/year2024/day23.rs
@@ -49,17 +49,20 @@ fn part1(input: Input) -> usize {
         .filter(|(_, values)| {
             return values.len() > 3;
         })
-        // Determine if any of the computer
-        .for_each(|(key, value_set)| {
-            for key2 in value_set.into_iter() {
-                if let Some(set2) = input.get(key2) {
-                    for key3 in set2.into_iter() {
-                        if value_set.contains(key3) {
-                            if &key[0..1] == "t" || &key2[0..1] == "t" || &key3[0..1] == "t" {
+        // Determine if any of the computers are groups of 3.
+        .for_each(|(address, value_set)| {
+            for address2 in value_set.into_iter() {
+                if let Some(set2) = input.get(address2) {
+                    for address3 in set2.into_iter() {
+                        if value_set.contains(address3) {
+                            if &address[0..1] == "t"
+                                || &address2[0..1] == "t"
+                                || &address3[0..1] == "t"
+                            {
                                 let mut set = BTreeSet::new();
-                                set.insert(key.to_string());
-                                set.insert(key2.to_string());
-                                set.insert(key3.to_string());
+                                set.insert(address.to_string());
+                                set.insert(address2.to_string());
+                                set.insert(address3.to_string());
                                 pairs.insert(set);
                             }
                         }
@@ -79,20 +82,21 @@ fn part2(input: Input) -> String {
     ) -> HashSet<String> {
         let mut set: HashSet<String> = current_set.clone();
 
-        'outer: for key in potential_group_connections.clone().iter() {
-            // if every key in current_set doesn't connect to key, skip this set.
-            // else add the key and recursively try the next. Make sure to remove key from potential_group_connections.
-            let key_set = input.get(key).unwrap();
+        'outer: for address in potential_group_connections.clone().iter() {
+            let address_set = input.get(address).unwrap();
 
-            for pair_key in current_set.iter() {
-                if key_set.get(pair_key).is_none() {
-                    potential_group_connections.remove(key);
+            for pair_address in current_set.iter() {
+                // If the address is not available by all values in the set, remove it so that
+                // we do less checks later. All it takes is one address to be missing from our current_set
+                // to invalidate a case.
+                if address_set.get(pair_address).is_none() {
+                    potential_group_connections.remove(address);
                     continue 'outer;
                 }
             }
 
             let mut new_current_set = current_set.clone();
-            new_current_set.insert(key.to_string());
+            new_current_set.insert(address.to_string());
 
             let result = recursive_search(&mut new_current_set, potential_group_connections, input);
 
@@ -104,40 +108,28 @@ fn part2(input: Input) -> String {
         return set;
     }
 
-    // Start the search on every key.
+    // Check if every address is part of the biggest group
     let values: Vec<HashSet<String>> = input
         .iter()
-        .map(|(key, value_set)| {
-            // Rules:
-            // - the LAN party cannot be bigger then the smallest set in the group
+        .map(|(address, value_set)| {
             let mut current_set: HashSet<String> = HashSet::new();
-            current_set.insert(key.to_string());
-
+            current_set.insert(address.to_string());
             return recursive_search(&mut current_set, &mut value_set.clone(), &input);
         })
         .collect();
 
+    // Find the addresses of the largest set
     let mut largest_set = values
         .iter()
         .max_by_key(|s| s.len())
         .unwrap()
         .iter()
-        .map(|key| key)
-        .collect::<Vec<&String>>();
+        .map(|address| address.as_str())
+        .collect::<Vec<&str>>();
 
+    // Alphabatetize the outputs and join them together
     largest_set.sort();
-
-    let output = largest_set.into_iter().fold(String::new(), |acc, value| {
-        if acc == "" {
-            return String::new() + value;
-        }
-
-        return acc + "," + &value;
-    });
-
-    println!("{:?}", output);
-
-    return output;
+    return largest_set.join(",");
 }
 
 pub fn run() {
